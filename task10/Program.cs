9eda8adec2752e7483b65fff601d7e4234933619
sergey <sergey.fomin.90@inbox.ru>
2022-08-09 21:04:31 +0400

    task10Сhanges

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 10: Напишите программу, которая принимает
+ на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.WriteLine("Введите трехзначное число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int SecondDigit = Number % 100 / 10;
+Console.WriteLine($"Второе число = {SecondDigit}");
